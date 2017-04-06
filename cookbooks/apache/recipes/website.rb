2b24c781bffac91104b5df2c6ac1519e804ce701
Gaja <gaja@gmail.com>
2017-04-06 06:03:29 +0000

    Added few cookbooks

--- cookbooks/apache/recipes/website.rb
+++ cookbooks/apache/recipes/website.rb
@@ -1,4 +1,8 @@
 file 'default www' do
 	path '/var/www/html/index.html'
-	content 'hello gaja'
+	content 'hello gaja1'
+end
+webnodes = search('node', 'role:web')
+webnodes.each do |node|
+	puts node
 end
